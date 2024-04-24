f2c5edcf41d4469711adf78adef7789fd6517b32
Raman <ramanmcreates@gmail.com>
2024-04-24 19:45:00 -0400

    updates

--- main.js
+++ main.js
@@ -86,22 +86,22 @@ tl.fromTo('.title', {opacity: 0}, {opacity: 1})
 
 //Mouse Anmiation Color
 
-let mouseDown = false
-let rgb = [12, 23, 55]
+// let mouseDown = false
+// let rgb = [12, 23, 55]
 
 
-window.addEventListener('mousedown', () =>(mouseDown = true))
-window.addEventListener('mouseup', () =>(mouseDown = false))
+// window.addEventListener('mousedown', () =>(mouseDown = true))
+// window.addEventListener('mouseup', () =>(mouseDown = false))
 
-window.addEventListener('mousemove', (e) =>{
-  if(mouseDown){
-    rgb = [
-      Math.round((e.pageX / sizes.width) * 255), 
-      Math.round((e.pageY / sizes.height) * 255),
-      150
-    ]
-    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
-    new THREE.Color((`rgb(0, 100, 150)`))
-    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b:newColor.b})
-  }
-})
+// window.addEventListener('mousemove', (e) =>{
+//   if(mouseDown){
+//     rgb = [
+//       Math.round((e.pageX / sizes.width) * 255), 
+//       Math.round((e.pageY / sizes.height) * 255),
+//       150
+//     ]
+//     let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+//     new THREE.Color((`rgb(0, 100, 150)`))
+//     gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b:newColor.b})
+//   }
+// })
