6c589578932fe28562ff9256cb71aebc287e9f20
Raman <ramanmcreates@gmail.com>
2024-04-28 21:08:11 -0400

    styling

--- main.js
+++ main.js
@@ -44,6 +44,31 @@ const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(2, 20, 20), new THREE
  
  scene.add(atmosphere)
 
+ const starGeometry = new THREE.BufferGeometry()
+
+ const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff
+ })
+
+ const starVertices = []
+
+
+ for (let i =0; i < 10000; i++){
+  const x = (Math.random() - .5) * 2000
+  const y = (Math.random() - .5) * 2000
+  const z = Math.random() * 2000
+  starVertices.push(x,y,z)
+ }
+
+ starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
+
+
+ console.log(starVertices)
+
+ const stars = new THREE.Points(starGeometry, starMaterial)
+
+ scene.add(stars)
+
 //Sizes 
 const sizes = {
   width: window.innerWidth, 
